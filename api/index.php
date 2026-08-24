751e11f8d5ea7bb20bac310394c15841bcac511b
YossecDesarrolloApp <desarrolloapp@girasol.pe>
2026-08-24 17:04:32 -0500

    fix: api/index.php autocontenido + includeFiles sin curly braces

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-require __DIR__ . '/../index.php';
+$html = file_get_contents(__DIR__ . '/../index.php');
+if ($html === false) {
+    http_response_code(500);
+    echo 'Error cargando pagina';
+    exit;
+}
+echo $html;
