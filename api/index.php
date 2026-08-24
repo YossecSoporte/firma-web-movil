a06203215876e6c10f4f28934163cf6e789208bc
YossecDesarrolloApp <desarrolloapp@girasol.pe>
2026-08-24 17:21:16 -0500

    Servir frontend como estatico index.html en Vercel

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-$html = file_get_contents(__DIR__ . '/../index.php');
-if ($html === false) {
-    http_response_code(500);
-    echo 'Error cargando pagina';
-    exit;
-}
-echo $html;
